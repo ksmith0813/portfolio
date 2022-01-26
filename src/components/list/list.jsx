effe9ffab9fb1231ec246d8abf58b6c860f0fa54
Kevin Smith <kevin.smith@capspire.com>
2022-01-26 16:35:04 -0600

    added user contant file, updated list to have a fixed header, replaced displayed user id with user name list values

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -4,6 +4,7 @@ import { MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons'
 import api from 'utils/api'
 import { arrayRemove, showMessage } from 'utils/general'
 import './list.scss'
+import { userNames } from 'constants/users'
 
 const { Option } = Select
 
@@ -11,14 +12,12 @@ export const List = () => {
   const [loading, setLoading] = useState(false)
   const [originalTodos, setOriginalTodos] = useState()
   const [todos, setTodos] = useState([])
-  const [userIds, setUserIds] = useState([])
 
   useEffect(() => {
     setLoading(true)
     api.getTodos().then(({ data }) => {
       setTodos(data)
       setOriginalTodos(data)
-      setUserIds([...new Set(data.map((d) => d.userId))])
       setLoading(false)
     })
   }, [])
@@ -62,9 +61,9 @@ export const List = () => {
         <Row justify='center' className='fs-150 text-center'>
           Things you need to do...
         </Row>
-        <div className='m-200'>
+        <Row justify='center' className='m-200'>
           <Search todos={originalTodos} setTodos={setTodos} setLoading={setLoading} />
-        </div>
+        </Row>
         <div className='todo-list-container'>
           {loading && (
             <div className='content-center'>
@@ -75,53 +74,58 @@ export const List = () => {
           {!loading && todos.length > 0 && (
             <>
               <Row className='bold border-bottom-light pb-100'>
-                <Col span={3}>Action</Col>
-                <Col span={3}>User Id</Col>
-                <Col span={15} className='pl-150'>
+                <Col span={1}>
+                  Action
+                </Col>
+                <Col span={5} className='pl-150'>
+                  User
+                </Col>
+                <Col span={15} className='pl-100'>
                   Title
                 </Col>
-                <Col span={3} className='pl-150'>
+                <Col span={3} className='pl-050'>
                   Completed
                 </Col>
               </Row>
-              {todos.map((t, i) => (
-                <Row key={t.id} className='pt-200'>
-                  <Col span={3} className='fs-150'>
-                    {i === 0 && <PlusCircleOutlined onClick={() => addTodo()} className='clickable add-item' />}
-                    {i > 0 && (
-                      <MinusCircleOutlined onClick={() => removeTodo(t.id)} className='clickable remove-item' />
-                    )}
-                  </Col>
-                  <Col span={3}>
-                    <Select
-                      value={t.userId}
-                      className='user-select'
-                      onChange={(value) => updateTodo({ ...t, userId: value })}
-                    >
-                      {userIds.map((u, i) => (
-                        <Option key={i} value={u}>
-                          {u}
-                        </Option>
-                      ))}
-                    </Select>
-                  </Col>
-                  <Col span={15} className='pl-150'>
-                    <Input
-                      className='ml-100'
-                      value={t.title}
-                      onChange={(e) => updateTodo({ ...t, title: e.target.value })}
-                    />
-                  </Col>
-                  <Col span={3} className='pl-150'>
-                    <Switch
-                      checked={t.completed}
-                      checkedChildren='Yes'
-                      unCheckedChildren='No'
-                      onClick={() => updateTodo({ ...t, completed: !t.completed })}
-                    />
-                  </Col>
-                </Row>
-              ))}
+              <div className='todo-list-items'>
+                {todos.map((t, i) => (
+                  <Row key={t.id} className='pt-200'>
+                    <Col span={1} className='fs-150'>
+                      {i === 0 && <PlusCircleOutlined onClick={() => addTodo()} className='clickable add-item' />}
+                      {i > 0 && (
+                        <MinusCircleOutlined onClick={() => removeTodo(t.id)} className='clickable remove-item' />
+                      )}
+                    </Col>
+                    <Col span={5} className='pl-150'>
+                      <Select
+                        value={userNames.filter((n) => n.id === t.userId)[0]?.value}
+                        className='user-select'
+                        onChange={(value) => updateTodo({ ...t, userId: value })}
+                      >
+                        {userNames.map((u, i) => (
+                          <Option key={i} value={u.id}>
+                            {u.value}
+                          </Option>
+                        ))}
+                      </Select>
+                    </Col>
+                    <Col span={15} className='pl-150'>
+                      <Input
+                        value={t.title}
+                        onChange={(e) => updateTodo({ ...t, title: e.target.value })}
+                      />
+                    </Col>
+                    <Col span={3} className='pl-150 pt-025'>
+                      <Switch
+                        checked={t.completed}
+                        checkedChildren='Yes'
+                        unCheckedChildren='No'
+                        onClick={() => updateTodo({ ...t, completed: !t.completed })}
+                      />
+                    </Col>
+                  </Row>
+                ))}
+              </div>
             </>
           )}
         </div>
@@ -143,7 +147,7 @@ const Search = ({ todos, setTodos }) => {
     const timeoutId = setTimeout(() => {
       if (searchChanged) {
         if (search) {
-          const matches = todos.filter((c) => c.title.toLowerCase().includes(search.toLowerCase()))
+          const matches = todos.filter((c) => c.userId && c.title.toLowerCase().includes(search.toLowerCase()))
           setTodos(matches)
         } else {
           setTodos(todos)
@@ -158,5 +162,9 @@ const Search = ({ todos, setTodos }) => {
     setSearch(e.target.value || '')
   }
 
-  return <Input onChange={onSearchChange} value={search} placeholder='Search for things to do' />
+  return (
+    <Col span={10}>
+      <Input onChange={onSearchChange} value={search} placeholder='Search for things to do' allowClear />
+    </Col>
+  )
 }
---
+++ src/constants/users.js
@@ -0,0 +1,12 @@
+export const userNames = [
+  { id: 1, value: 'Jerry Garcia' },
+  { id: 2, value: 'Bob Weir' },
+  { id: 3, value: 'Phil Lesh' },
+  { id: 4, value: 'Bill Kreutzmann' },
+  { id: 5, value: 'Mickey Hart' },
+  { id: 6, value: 'Ron McKernan' },
+  { id: 7, value: 'Keith Godchaux' },
+  { id: 8, value: 'Brent Mydland' },
+  { id: 9, value: 'Donna Jean Godchaux' },
+  { id: 10, value: 'Vince Welnick' },
+]
