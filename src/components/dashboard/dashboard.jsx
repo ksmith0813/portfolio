4f349d404d177c234dbd847c1a4d95ffa5c0ba7f
Kevin Smith <kevin.smith@capspire.com>
2022-01-26 18:07:20 -0600

    udpated dashboard to show antd result while it is being built :)

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,15 @@
+import { Result } from 'antd'
 import React from 'react'
 import './dashboard.scss'
 
 export const Dashboard = () => {
-  return <div className='page-center fs-300'>Build Dashboard</div>
+  return (
+    <div className='page-center fs-300'>
+      <Result
+        status='404'
+        title='Under Construction'
+        subTitle='Sorry, the page you visited is currently being built.'
+      />
+    </div>
+  )
 }
