cc3d81d1560d09dee508eb2ce302ffc8e8d0e304
Kevin Smith <kevin.smith@capspire.com>
2022-01-27 15:10:18 -0600

    organized functions in my location component

--- src/components/dashboard/widgets/myLocation.jsx
+++ src/components/dashboard/widgets/myLocation.jsx
@@ -2,19 +2,6 @@ import React from 'react'
 import { Card, Col } from 'antd'
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps'
 
-const Map = () => (
-  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 36.06258, lng: -94.15743 }}>
-    <Marker
-      position={{
-        lat: 36.06258,
-        lng: -94.15743,
-      }}
-    />
-  </GoogleMap>
-)
-
-const MapComponent = withScriptjs(withGoogleMap(Map))
-
 export const MyLocation = () => (
   <Col span={6}>
     <Card title='My Location - Fayetteville, AR'>
@@ -29,3 +16,16 @@ export const MyLocation = () => (
     </Card>
   </Col>
 )
+
+const Map = () => (
+  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 36.06258, lng: -94.15743 }}>
+    <Marker
+      position={{
+        lat: 36.06258,
+        lng: -94.15743,
+      }}
+    />
+  </GoogleMap>
+)
+
+const MapComponent = withScriptjs(withGoogleMap(Map))
