fcabe7ccaa30a8cbf896ebc657a46400f98d3a2a
Kevin Smith <kevin.smith@capspire.com>
2022-01-29 23:22:56 -0600

    a few markup, style updates to home page tiles

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { LoadingOutlined } from '@ant-design/icons'
 import dashboard from 'assets/dashboard.svg'
 import register from 'assets/register.svg'
 import grid from 'assets/grid.svg'
@@ -8,6 +7,7 @@ import list from 'assets/list.svg'
 import shop from 'assets/shop.svg'
 import visuals from 'assets/chart.svg'
 import './home.scss'
+import { Spin } from 'antd'
 
 export const Home = () => {
   const [activePage, setActivePage] = useState()
@@ -20,7 +20,7 @@ export const Home = () => {
 
   const Tile = ({ title, image, className = 'default' }) => (
     <div className='tile clickable' onClick={() => goToPage(title.toLowerCase())}>
-      <div className='fs-175 p-050'>{activePage === title.toLowerCase() ? <LoadingOutlined /> : title}</div>
+      <div className='fs-175 p-050'>{activePage === title.toLowerCase() ? <Spin /> : title}</div>
       <img src={image} className={`tile-icon ${className}`} alt='' />
     </div>
   )
@@ -32,7 +32,7 @@ export const Home = () => {
           <div className='title'>Hello.</div>
           <div className='description'>Welcome to my React portfolio app.</div>
         </div>
-        <div className='flex p-200 pt-300'>
+        <div className='tile-container'>
           <Tile title='Dashboard' image={dashboard} />
           <Tile title='Register' image={register} className='register' />
           <Tile title='Grid' image={grid} className='grid' />
