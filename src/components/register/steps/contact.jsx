44672ba697e46eac5b79e98b865421621be413f6
Kevin Smith <kevin.smith@capspire.com>
2022-05-05 13:25:59 -0500

    updated state contact control to be an autocomplete

--- src/components/register/steps/contact.jsx
+++ src/components/register/steps/contact.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
-import { Form, Input, Select, Row, Col } from 'antd'
+import { Form, Input, Row, Col, AutoComplete } from 'antd'
 import { validateZip } from 'components/_siteWide/form/validators/validateZip'
 import { validatePhone } from 'components/_siteWide/form/validators/validatePhone'
 import { validateEmail } from 'components/_siteWide/form/validators/validateEmail'
@@ -10,13 +10,19 @@ import { states } from 'constants/states'
 import { setClean, setContact, getState, nextStep } from 'store/slices/registerSlice'
 import { Actions } from './actions'
 
-const { Option } = Select
 const inputField = FormItem(Input)
-const selectField = FormItem(Select)
+const autocompleteField = FormItem(AutoComplete)
 
 const Contact = () => {
   const state = useSelector(getState)
   const dispatch = useDispatch()
+
+  const statesValues = [...new Set(states.map((s) => s).sort())].map((v) => {
+    return { value: v.value }
+  })
+
+  const [filteredStates, setFilteredState] = useState(statesValues)
+
   const contact = state.contact
   const clean = state.clean
   const [form] = Form.useForm()
@@ -94,18 +100,23 @@ const Contact = () => {
               <Field
                 name='State'
                 defaultValue={contact.State}
-                component={selectField}
+                component={autocompleteField}
                 onChange={(value) => dispatch(setContact({ ...contact, State: value }))}
+                onSearch={(value) => {
+                  if (value) {
+                    const matches = states.filter((s) =>
+                      s.value.toLowerCase().trim().includes(value.toLowerCase().trim())
+                    )
+                    setFilteredState(matches)
+                  } else {
+                    setFilteredState(states)
+                  }
+                }}
+                options={filteredStates}
                 initialValues={clean}
                 required
                 hasFeedback
-              >
-                {states.map((s) => (
-                  <Option key={s.code} value={s.value}>
-                    {s.value}
-                  </Option>
-                ))}
-              </Field>
+              />
             </Col>
             <Col span={6} className='pl-100 pt-050'>
               <Field
