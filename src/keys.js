d1b19a0b353ceae28f461973c04c70b706de3894
Kevin Smith <kevin.smith@capspire.com>
2022-05-05 10:21:29 -0500

    added a few keys

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 export const keys = {
-  movieApi: '',
-  weatherApi: '',
+  movieApi: '5263fce',
+  weatherApi: 'e2c2951ed12e4f79b7624806210108',
   mapUrl: 'https://maps.googleapis.com/maps/api/js?key=#&v=3.exp&libraries=geometry,drawing,places',
 }
