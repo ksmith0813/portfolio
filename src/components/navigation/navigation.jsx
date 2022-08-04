92e036bab429e838b821a1a159545ab06f5793fc
Kevin Smith <ksmith0813@gmail.com>
2022-08-04 15:00:48 -0500

    updated navigation to latest antd menu usage

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -23,7 +23,6 @@ import signature from 'assets/signature.png'
 import './navigation.scss'
 
 const { Header, Sider, Content } = Layout
-const { SubMenu } = Menu
 
 export const Navigation = () => {
   const [activePage, setActivePage] = useState('home')
@@ -40,57 +39,90 @@ export const Navigation = () => {
 
   const SiteLink = ({ page, title }) => <Link to={`/${page}`}>{title}</Link>
 
-  const menu = (
-    <Menu mode='inline' selectedKeys={[activePage]} className={`site-nav ${state.selectedTheme}`}>
-      <Menu.Item key='home' icon={<HomeOutlined />}>
-        <SiteLink page='home' title='Home' />
-      </Menu.Item>
-      <Menu.Item key='dashboard' icon={<LayoutOutlined />}>
-        <SiteLink page='dashboard' title='Dashboard' />
-      </Menu.Item>
-      <Menu.Item key='register' icon={<FormOutlined />}>
-        <SiteLink page='register' title='Register' />
-      </Menu.Item>
-      <Menu.Item key='grid' icon={<TableOutlined />}>
-        <SiteLink page='grid' title='Grid' />
-      </Menu.Item>
-      <Menu.Item key='video' icon={<VideoCameraOutlined />}>
-        <SiteLink page='video' title='Video' />
-      </Menu.Item>
-      <Menu.Item key='list' icon={<UnorderedListOutlined />}>
-        <SiteLink page='list' title='List' />
-      </Menu.Item>
-      <Menu.Item key='tree' icon={<NodeExpandOutlined />}>
-        <SiteLink page='tree' title='Tree' />
-      </Menu.Item>
-      <Menu.Item key='search/media' icon={<CustomerServiceOutlined />}>
-        <SiteLink page='search/media' title='Media' />
-      </Menu.Item>
-      <Menu.Item key='search/weather' icon={<RadarChartOutlined />}>
-        <SiteLink page='search/weather' title='Weather' />
-      </Menu.Item>
-      <Menu.Item key='shop' icon={<ShoppingOutlined />}>
-        <SiteLink page='shop' title='Shop' />
-      </Menu.Item>
-      <Menu.Item key='visuals' icon={<BarChartOutlined />}>
-        <SiteLink page='visuals' title='Visuals' />
-      </Menu.Item>
-      <Menu.Item key='bio' icon={<UserOutlined />}>
-        <SiteLink page='bio' title='Bio' />
-      </Menu.Item>
-      <SubMenu key='theme' title='Theme' icon={<BgColorsOutlined />}>
-        <Menu.Item key='blue' onClick={() => dispatch(setSelectedTheme('default'))}>
-          Blue
-        </Menu.Item>
-        <Menu.Item key='green' onClick={() => dispatch(setSelectedTheme('green'))}>
-          Green
-        </Menu.Item>
-        <Menu.Item key='purple' onClick={() => dispatch(setSelectedTheme('purple'))}>
-          Purple
-        </Menu.Item>
-      </SubMenu>
-    </Menu>
-  )
+  const menuItems = [
+    {
+      key: 'home',
+      icon: <HomeOutlined />,
+      label: <SiteLink page='home' title='Home' />,
+    },
+    {
+      key: 'dashboard',
+      icon: <LayoutOutlined />,
+      label: <SiteLink page='dashboard' title='Dashboard' />,
+    },
+    {
+      key: 'register',
+      icon: <FormOutlined />,
+      label: <SiteLink page='register' title='register' />,
+    },
+    {
+      key: 'grid',
+      icon: <TableOutlined />,
+      label: <SiteLink page='grid' title='grid' />,
+    },
+    {
+      key: 'video',
+      icon: <VideoCameraOutlined />,
+      label: <SiteLink page='video' title='Video' />,
+    },
+    {
+      key: 'list',
+      icon: <UnorderedListOutlined />,
+      label: <SiteLink page='list' title='List' />,
+    },
+    {
+      key: 'tree',
+      icon: <NodeExpandOutlined />,
+      label: <SiteLink page='tree' title='Tree' />,
+    },
+    {
+      key: 'search/media',
+      icon: <CustomerServiceOutlined />,
+      label: <SiteLink page='search/media' title='Media' />,
+    },
+    {
+      key: 'search/weather',
+      icon: <RadarChartOutlined />,
+      label: <SiteLink page='search/weather' title='Weather' />,
+    },
+    {
+      key: 'shop',
+      icon: <ShoppingOutlined />,
+      label: <SiteLink page='shop' title='Shop' />,
+    },
+    {
+      key: 'visuals',
+      icon: <BarChartOutlined />,
+      label: <SiteLink page='visuals' title='Visuals' />,
+    },
+    {
+      key: 'bio',
+      icon: <UserOutlined />,
+      label: <SiteLink page='bio' title='Bio' />,
+    },
+    {
+      key: 'theme',
+      icon: <BgColorsOutlined />,
+      label: <SiteLink page='theme' title='Theme' />,
+      children: [
+        {
+          key: 'blue',
+          label: 'Blue',
+          onClick: () => dispatch(setSelectedTheme('default')),
+        },
+        {
+          key: 'green',
+          label: 'Green',
+          onClick: () => dispatch(setSelectedTheme('green')),
+        },
+        {
+          key: 'purple',
+          label: 'Purple',
+          onClick: () => dispatch(setSelectedTheme('purple')),
+        },
+      ],
+    },
+  ]
 
   return (
     <Layout className='app-layout'>
@@ -98,7 +130,12 @@ export const Navigation = () => {
         <div className='logo'>
           <Avatar src={kevin}></Avatar>
         </div>
-        {menu}
+        <Menu
+          mode='inline'
+          selectedKeys={[activePage]}
+          className={`site-nav ${state.selectedTheme}`}
+          items={menuItems}
+        />
       </Sider>
       <Layout>
         <Header className='site-header'>
