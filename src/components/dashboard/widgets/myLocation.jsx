f975057588ef54cb8d9c21dd0a072f6424f19d17
Kevin Smith <ksmith0813@gmail.com>
2023-03-16 12:28:14 -0500

    added city, state to my location widget

--- src/components/dashboard/widgets/myLocation.jsx
+++ src/components/dashboard/widgets/myLocation.jsx
@@ -4,7 +4,7 @@ import { MapLocation } from 'components/_siteWide/layout/layout'
 
 export const MyLocation = () => (
   <Col span={6}>
-    <Card title='My Location'>
+    <Card title='My Location - Fayetteville, AR'>
       <MapLocation location={{ lat: 36.08689883375299, lon: -94.21634189949994 }} containerClass='card-display' />
     </Card>
   </Col>
